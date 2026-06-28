b381bad2c3c030820bea6e29a74e2b3e375a07db
Abhi-Salunke <salunkeabhijit39@gmail.com>
2026-06-29 01:17:23 +0530

    Break and continue stetement

---
+++ day 17/Break & Continue.py
@@ -0,0 +1,17 @@
+#for i in range(12):
+   # print ("5 X",i, "=",5 * i)
+  #  if (i ==10):
+ #       break
+#print ("Loop ko chodkar nikal gaya")
+
+for i in range(12):
+    if (i==10):
+        print("skip the iteration ")
+        continue
+    print("5 X",i,"=",5 * i)
+
+# in  this break and continue statement 
+# break statement madhe apan code stop 
+# karun dusara code suru karu sakto
+# and continue statement mdhe toch code suru karu sakto
+#  
